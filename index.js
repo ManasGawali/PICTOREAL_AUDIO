3f4e6f611aba6e80ea6888ac3b476ca55b4106dd
ManasGawali <manasmgawali@gmail.com>
2025-05-24 22:09:02 +0530

    final changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = 5000;
 const cors = require('cors');
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://www.pictoreal.in",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     optionsSuccessStatus: 200,
 }));
